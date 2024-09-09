b41aa4b2c7758dfdb23c578d6db0b572f877d06e
Mohamed SayedAhmed Hassani <mhassani@hemmahq.com>
2024-09-09 22:58:48 +0300

    add main repository

--- Devlance.Domain/Interfaces/Repositories/IRepository.cs
+++ Devlance.Domain/Interfaces/Repositories/IRepository.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Devlance.Domain.Interfaces.Repositories
 {
-    public interface IRepository<TEntity>
+    public interface IRepository<TEntity,TKey>
     {
-        TEntity GetById(int id);
+        TEntity Find(TKey id);
+
+        int Add(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> GetList(Expression<Func<TEntity, bool>> expression);
 
 
     }
--- Devlance.Infrastructure/Repositories/Repository.cs
+++ Devlance.Infrastructure/Repositories/Repository.cs
@@ -3,12 +3,13 @@ using Devlance.Infrastructure.DbContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Devlance.Infrastructure.Repositories
 {
-    public class Repository<T> : IRepository<T> where T:class
+    public class Repository<T,TKey> : IRepository<T,TKey> where T:class
     {
         private readonly DevlanceContext _dbContext;
         public Repository(DevlanceContext dbContext )
@@ -16,9 +17,37 @@ namespace Devlance.Infrastructure.Repositories
             _dbContext = dbContext;
         }
 
-        public T GetById(int id)
+        public int Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public T Find(TKey id)
         {
             return _dbContext.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbContext.Set<T>();
+        }
+
+        public IQueryable<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            return _dbContext.Set<T>().Where(expression);
+        }
+
+        public int Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            return _dbContext.SaveChanges();
         }
     }
 }
